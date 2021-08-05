f16d119d95ac3d69cb95f5811ae7405661f85188
Tasco Silva <tascosilva@tasco.home>
2021-08-05 14:38:45 +0200

    Add products app, models and fixtures.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -148,6 +149,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
