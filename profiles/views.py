22dbff486c6c4d683f9cfbbd09aba0a30f8b2ba5
Tasco Silva <tascosilva@tasco.home>
2021-08-26 10:35:43 +0200

    Securing the views.

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,15 @@
 from django.db.models.query import InstanceCheckMeta
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
+
+@login_required
 def profile(request):
     """
     Display the user's profile.
