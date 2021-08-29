ddac26405ebeb23a52b6e195cfffc78d33d6c227
Tasco Silva <tascosilva@tasco.home>
2021-08-29 13:05:24 +0200

    Connect Django to S3.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from custom_storages import MediaStorage, StaticStorage
+# from custom_storages import MediaStorage, StaticStorage
 import os
 import dj_database_url
 
@@ -179,7 +179,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
