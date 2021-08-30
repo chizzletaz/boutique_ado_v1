13f528e85dd2a286aaa53c40e1d0573a4e47a9f7
Tasco Silva <tascosilva@tasco.home>
2021-08-30 14:28:37 +0200

    Refactor base.css and login/reg.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['chizzletaz-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['chizzletaz-boutique-ado.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
